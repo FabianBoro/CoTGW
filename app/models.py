a9735e3982919517ce708ef357008db7c725c0f3
FabianBoro <fabian.boro.fb@gmail.com>
2025-07-15 20:20:22 +0700

    -

--- app/models.py
+++ app/models.py
@@ -12,6 +12,7 @@ class Device(Base):
     device_id = Column(String, unique=True, index=True) 
     name = Column(String, default="Unnamed Device")
     last_seen = Column(DateTime, default=datetime.utcnow)
+    callsign = Column(String, nullable=True)
 
     #relasi dengan loc
     locations = relationship("Location", back_populates="device")
